d89efed8a11fce7a354cacabd29c71641740c8af
Marco Kemmerling <marco@kemmerling.me>
2016-09-19 18:55:50 +0200

    angular lab list on home page

--- labs/js/controllers.js
+++ labs/js/controllers.js
@@ -1,6 +1,6 @@
 /**
- * Created by mar on 20-2-16.
- */
+* Created by mar on 20-2-16.
+*/
 'use strict';
 
 /* Controllers */
@@ -9,17 +9,17 @@ var labsviewApp = angular.module('labsviewApp', []);
 
 labsviewApp.controller('LabListCtrl', function($scope) {
     $scope.labs = [
-        {'name': 'The Simplex Method',
-         'category': 'Linear Programming',
-         'description': 'The all powerful Simplex method.',
-         'url': 'simplex/'},
+        {'name': 'The Smith-Waterman Algorithm',
+        'category': 'Bioinformatics',
+        'description': 'A dynamic programming algorithm for local sequence alignment.',
+        'url': 'smith-waterman/'},
         {'name': 'The Needleman-Wunsch Algorithm',
-         'category': 'Bioinformatics',
-         'description': 'A dynamic programming algorithm for global sequence alignment.',
-         'url': 'needleman-wunsch/'},
-        {'name': 'The Smith-Waterman Algorithm', 
-         'category': 'Bioinformatics', 
-         'description': 'A dynamic programming algorithm for local sequence alignment.', 
-         'url': 'smith-waterman/'}
+        'category': 'Bioinformatics',
+        'description': 'A dynamic programming algorithm for global sequence alignment.',
+        'url': 'needleman-wunsch/'},
+        {'name': 'The Simplex Method',
+        'category': 'Linear Programming',
+        'description': 'The all powerful Simplex method.',
+        'url': 'simplex/'},
     ];
 });

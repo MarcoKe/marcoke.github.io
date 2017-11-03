57373b47a7bb7193ca812c4eb7c6556c96465830
Marco Kemmerling <marco@kemmerling.me>
2017-11-03 13:45:33 +0100

    update navbar

---
+++ notes/internship/navbar.js
@@ -0,0 +1,7 @@
+document.getElementById("navMenu").innerHTML =
+'<div class="container">' +
+    '<ul class="navbar-list">' +
+        '<li class="navbar-item"><a class="navbar-link" href="http://kemmerling.me/notes/internship">Internship Notes Home</a></li>' +
+        
+    '</ul>' +
+'</div>';

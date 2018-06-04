04c3af6196968bcf1a8602fc68db437066a11afd
Marco Kemmerling <marco@kemmerling.me>
2018-06-04 20:05:56 +0200

    remove space from directory name

---
+++ projects/navbar.js
@@ -0,0 +1,9 @@
+document.getElementById("navMenu").innerHTML =
+'<div class="container">' +
+    '<ul class="navbar-list">' +
+        '<li class="navbar-item"><a class="navbar-link" href="http://kemmerling.me">Home</a></li>' +
+        '<li class="navbar-item"><a class="navbar-link" href="http://kemmerling.me/#labs">Labs</a></li>' +
+        '<li class="navbar-item"><a class="navbar-link" href="http://kemmerling.me/#contact">Contact</a></li>' +
+        '<li class="navbar-item"><a class="navbar-link" href="http://kemmerling.me/#">More</a></li>'+
+    '</ul>' +
+'</div>';
